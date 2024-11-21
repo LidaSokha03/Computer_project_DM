5e799db0b630bc8399c4e6813633db9303a35d76
Maxim Holovin <holovin.pn@ucu.edu.ua>
2024-11-21 14:13:13 +0200

    find bridges function implemented

--- main/articulation.py
+++ main/articulation.py
@@ -5,9 +5,30 @@ def find_points(graph):
     ...
 
 
-def fing_bridges(graph):
+def bridges(graph):
     """
-    ...
+    Find all bridges in an undirected graph.
     """
-    ...
+    bridges_list = []
+    copied_graph = graph.copy()
+    for edge in copied_graph:
+        graph.pop(edge)
+        values = []
+        for key in graph:
+            for value in graph.get(key):
+                values.append(value)
+            values.append(key)
 
+        for edge_value in copied_graph.get(edge):
+            if edge_value in graph:
+                pass
+            else:
+                if edge in copied_graph.get(edge_value):
+                    tuple_add = (edge_value, edge)
+                    bridges_list.append(tuple_add)
+                else:
+                    tuple_add = (edge, edge_value)
+                    bridges_list.append(tuple_add)
+
+
+    return bridges_list
